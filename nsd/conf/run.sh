1f523797f35d2eb4501d9f41018d6ca1068cfb24
kometchtech <kometch@gmail.com>
2018-03-02 23:58:54 +0900

    add run.sh

---
+++ nsd/conf/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ ! -f /usr/local/etc/nsd_server.pem ]; then
+  nsd-control-setup
+fi
+
+chown -R nsd:nsd /usr/local/etc/nsd /usr/local/etc/nsd/zones /usr/local/var/db/nsd /var/run/nsd
+
+/usr/local/sbin/nsd -d -P /var/run/nsd/nsd.pid
