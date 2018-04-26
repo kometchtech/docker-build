700a348c38043d36d1aecd74ed5198736af59397
kometchtech <5137077+kometchtech@users.noreply.github.com>
2018-04-26 21:25:45 +0900

    fix 1.7.1rc1 fix CFLAGS fix CXXFLAGS fix LDFLAGS

--- unbound/run.sh
+++ unbound/run.sh
@@ -7,4 +7,8 @@ fi
 # change owner
 chown -R unbound:unbound /etc/unbound
 
-/usr/local/sbin/unbound -d -c /etc/unbound/unbound.conf
+if [ -f /etc/unbound/unbound.conf ]; then
+  /usr/local/sbin/unbound -d -c /etc/unbound/unbound.conf
+else
+  /usr/local/sbin/unbound -d
+fi
