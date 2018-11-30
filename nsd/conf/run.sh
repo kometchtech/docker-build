afbefedbe579ca62f9ea500d9fe92eba173a06ee
kometchtech <kometch@gmail.com>
2018-11-30 22:38:16 +0900

    fix Dockerfile fot NSD
    - add sysconfdir.
    - The config file is specified at run time.

--- nsd/conf/run.sh
+++ nsd/conf/run.sh
@@ -6,4 +6,4 @@ fi
 
 chown -R nsd:nsd /usr/local/etc/nsd /usr/local/etc/nsd/zones /usr/local/var/db/nsd /var/run/nsd
 
-/usr/local/sbin/nsd -d -P /var/run/nsd/nsd.pid
+/usr/local/sbin/nsd -d -P /var/run/nsd/nsd.pid -c /etc/nsd/nsd.conf
