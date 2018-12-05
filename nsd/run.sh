155c4ce779ff4864ae20df546615abd6c1c690dc
kometchtech <kometch@gmail.com>
2018-12-05 21:20:44 +0900

    fix Dockerfile for NSD
    * update to 4.1.26
    
    * fix run.sh
    
     * checks the existence of the setting file.

---
+++ nsd/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ ! -f /usr/local/etc/nsd_server.pem ]; then
+  nsd-control-setup
+fi
+
+chown -R nsd:nsd /usr/local/etc/nsd /usr/local/etc/nsd/zones /usr/local/var/db/nsd /var/run/nsd
+
+if [ -f /etc/nsd/nsd.conf ]; then
+	/usr/local/sbin/nsd -d -P /var/run/nsd/nsd.pid -c /etc/nsd/nsd.conf
+else
+	/usr/local/sbin/nsd -d -P /var/run/nsd/nsd.pid
+fi
+~                
