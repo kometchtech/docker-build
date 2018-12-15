484b1b3a97cefdb39b503960cd4cbc192c645927
kometchtech <kometch@gmail.com>
2018-12-15 11:27:57 +0900

    rmdir conf/

--- nsd/conf/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -f /usr/local/etc/nsd_server.pem ]; then
-  nsd-control-setup
-fi
-
-chown -R nsd:nsd /usr/local/etc/nsd /usr/local/etc/nsd/zones /usr/local/var/db/nsd /var/run/nsd
-
-/usr/local/sbin/nsd -d -P /var/run/nsd/nsd.pid -c /etc/nsd/nsd.conf
