5c1285753f400bb5122206f6b5c75e23de0912d8
kometchtech <kometch@gmail.com>
2019-02-09 18:04:35 +0900

    fix
    * modified it because I made a mistake in how to write "WORKDIR" in
    Dockerfile.

--- nsd/run.sh
+++ nsd/run.sh
@@ -3,8 +3,8 @@
 if [ ! -f /usr/local/etc/nsd_server.pem ]; then
   nsd-control-setup
 fi
-
-chown -R nsd:nsd /usr/local/etc/nsd /usr/local/etc/nsd/zones /usr/local/var/db/nsd /var/run/nsd
+# change owner and group
+#chown -R nsd:nsd /usr/local/etc/nsd /usr/local/etc/nsd/zones /usr/local/var/db/nsd /var/run/nsd
 
 if [ -f /etc/nsd/nsd.conf ]; then
 	/usr/local/sbin/nsd -d -P /var/run/nsd/nsd.pid -c /etc/nsd/nsd.conf
