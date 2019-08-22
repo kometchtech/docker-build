28e090819862428d8e67cdeb775c76cf183dab58
kometchtech <kometch@gmail.com>
2019-08-23 00:31:50 +0900

    :heavy_exclamation_mark: update kea: version 1.6.0-beta2

--- kea/run.sh
+++ kea/run.sh
@@ -1,10 +1,10 @@
 #!/bin/ash
 
-if [ -e /usr/var/kea/kea-dhcp4.kea-dhcp4.pid ]; then
-    rm -f /usr/var/kea/kea-dhcp4.kea-dhcp4.pid
+if [ -e /usr/var/run/kea/kea-dhcp4.kea-dhcp4.pid ]; then
+    rm -f /usr/var/run/kea/kea-dhcp4.kea-dhcp4.pid
 fi
-if [ -e /usr/var/kea/kea-dhcp6.kea-dhcp6.pid ]; then
-    rm -f /usr/var/kea/kea-dhcp6.kea-dhcp6.pid
+if [ -e /usr/var/run/kea/kea-dhcp6.kea-dhcp6.pid ]; then
+    rm -f /usr/var/run/kea/kea-dhcp6.kea-dhcp6.pid
 fi
 
 DEBUG=""
