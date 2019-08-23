e5900812716d2c3add4cc2b556bafb48720f34aa
kometchtech <kometch@gmail.com>
2019-08-23 10:21:23 +0900

    fix run.sh for ISC kea.
    * added the procps package to run keactrl.
    * Fixed the run.sh so that keactrl would quit if you left it as it was.

--- kea/run.sh
+++ kea/run.sh
@@ -7,31 +7,5 @@ if [ -e /usr/var/run/kea/kea-dhcp6.kea-dhcp6.pid ]; then
     rm -f /usr/var/run/kea/kea-dhcp6.kea-dhcp6.pid
 fi
 
-DEBUG=""
-
-while getopts "4:6:d" opts
-do
-    case $opts in
-        4)
-            if [ ! -f $OPTARG ]; then
-                exit 1
-            else
-                nohup sh -c "kea-dhcp4 -c $OPTARG $DEBUG" &
-            fi
-            ;;
-        6)
-            if [ ! -f $OPTARG ]; then
-                exit 1
-            else
-                nohup sh -c "kea-dhcp6 -c $OPTARG $DEBUG" &
-            fi
-            ;;
-        d)
-            DEBUG="-d"
-            ;;
-        *)
-            exit 1
-            ;;
-    esac
-done
+/usr/sbin/keactrl start && tail -f /dev/null
 wait
