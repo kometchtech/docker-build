7d8da13aacaf2a756a297402f3e7cfdbf2193dde
kometchtech <kometch@gmail.com>
2019-08-24 13:00:03 +0900

    code refactoring
    * Fix FLAG related.
    * Fix entrypoint script.

---
+++ nsd/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env ash
+
+if [ ! -f /usr/local/etc/nsd_server.pem ]; then
+  nsd-control-setup
+fi
+# change owner and group
+#chown -R nsd:nsd /usr/local/etc/nsd /usr/local/etc/nsd/zones /usr/local/var/db/nsd /var/run/nsd
+
+if [ ! -f /etc/nsd/nsd.conf ]; then
+	/usr/local/sbin/nsd -d -P /var/run/nsd/nsd.pid $@
+else
+	/usr/local/sbin/nsd -d -P /var/run/nsd/nsd.pid -c /etc/nsd/nsd.conf
+fi
