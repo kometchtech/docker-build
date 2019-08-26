ec7bcdc1b61f2dc05ef3f23c5ac379bd891c427c
kometchtech <kometch@gmail.com>
2019-08-26 21:28:39 +0900

    :heavy_exclamation_mark: update kea: version 1.6.0-beta2

--- kea/entrypoint.sh
+++ kea/entrypoint.sh
@@ -1,11 +1,15 @@
-#!/bin/ash
+#!/usr/bin/env sh
 
-if [ -e /usr/local/var/run/kea/kea-dhcp4.kea-dhcp4.pid ]; then
-    rm -f /usr/local/var/run/kea/kea-dhcp4.kea-dhcp4.pid
+
+RUNPATH="/usr/local/var/run/kea"
+
+if [ -e "${RUNPATH}/kea-dhcp4.kea-dhcp4.pid" ]; then
+    rm -f "${RUNPATH}/kea-dhcp4.kea-dhcp4.pid"
 fi
-if [ -e /usr/local/var/run/kea/kea-dhcp6.kea-dhcp6.pid ]; then
-    rm -f /usr/local/var/run/kea/kea-dhcp6.kea-dhcp6.pid
+if [ -e "${RUNPATH}/kea-dhcp6.kea-dhcp6.pid" ]; then
+    rm -f "${RUNPATH}/kea-dhcp6.kea-dhcp6.pid"
 fi
 
-/usr/local/sbin/keactrl start -c /etc/kea/keactrl.conf
+sh -c  "keactrl start -c /etc/kea/keactrl.conf"
+
 tail -f /dev/null
