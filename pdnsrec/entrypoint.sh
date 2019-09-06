d60689794b48ac4e31e32d8e1b29a6e8df934363
kometchtech <kometch@gmail.com>
2019-09-06 09:25:37 +0900

    :heavy_exclamation_mark: update pdnsrec: version 4.3.0-alpha1

--- pdnsrec/entrypoint.sh
+++ pdnsrec/entrypoint.sh
@@ -3,7 +3,7 @@
 mkdir -p /var/run/pdns-recursor/
 
 if [ ! -f /etc/powerdns/recursor.conf ]; then
-    pdns_recursor --disable-syslog --log-timestamp=yes --daemon=no --write-pid=no -c /etc/powerdns/recursor.conf
+    pdns_recursor --disable-syslog --log-timestamp=yes --daemon=no --write-pid=no --config /etc/powerdns/recursor.conf
 else
     pdns_recursor --disable-syslog --log-timestamp=yes --daemon=no --write-pid=no
 fi
