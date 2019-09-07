188e90e440e7cd91c7e5bd93319fe011420bc862
kometchtech <kometch@gmail.com>
2019-09-07 12:42:43 +0900

    :heavy_exclamation_mark: update pdnsrec: version 4.3.0-alpha1

--- pdnsrec/entrypoint.sh
+++ pdnsrec/entrypoint.sh
@@ -3,7 +3,7 @@
 mkdir -p /var/run/pdns-recursor/
 
 if [ -f /etc/powerdns/recursor.conf ]; then
-    pdns_recursor --daemon=no --write-pid=no --config /etc/powerdns/recursor.conf $@
+    pdns_recursor --daemon=no --write-pid=no --config /etc/powerdns/recursor.conf "$@"
 else
-    pdns_recursor --daemon=no --write-pid=no $@
+    pdns_recursor --daemon=no --write-pid=no "$@"
 fi
