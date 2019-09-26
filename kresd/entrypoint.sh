5852c83d605e263e3cc1c037dcec8945b58acc24
kometchtech <kometch@gmail.com>
2019-09-26 22:42:06 +0900

    :heavy_exclamation_mark: update kresd: version 4.2.1

--- kresd/entrypoint.sh
+++ kresd/entrypoint.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env sh
 
+set -e
+
+ROOTKEY=${ROOTKEY:-"/var/cache/knot-resolver/root.keys"}
+CACHE=${CACHE:-"/var/cache/knot-resolver"}
+
 # Deleting the trust anchor key file
-rm -f /var/cache/knot-resolver/root.keys
+if [ -e ${ROOTKEY} ]; then
+    rm -f ${ROOTKEY}
+fi
 # Process execution
 var="$@"
 # Knot Resolver Garbage Collector daemon
-kres-cache-gc -c /var/cache/knot-resolver -d 1000 &
+kres-cache-gc -c ${CACHE} -d 1000 &
 # Knot Resolver daemon
-sh -c "/usr/sbin/kresd $var" 
+/usr/sbin/kresd ${var}
 # process foreground
-tail -f /dev/null
+fg %1
