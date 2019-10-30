9fccdaa57abb2227ae7be5ceec7d0219f1b42558
kometchtech <kometch@gmail.com>
2019-10-30 19:03:40 +0900

    :heavy_exclamation_mark: update pdnsrec: version 4.3.0-alpha3

--- pdnsrec/entrypoint.sh
+++ pdnsrec/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
+set -e
+
 mkdir -p /var/run/pdns-recursor/
 
 var="$@"
-echo $var
+#echo $var
 
 if [ -f /etc/powerdns/recursor.conf ]; then
     pdns_recursor --disable-syslog --daemon=no --write-pid=no $var
