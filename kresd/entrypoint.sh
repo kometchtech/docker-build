71cc44d0136c78d4a0ae9b515dea59c8e862ab84
kometchtech <kometch@gmail.com>
2021-07-31 10:00:43 +0900

    update version 5.4.0
    * Change base image to debian
    * Fix entrypoint.sh

--- kresd/entrypoint.sh
+++ kresd/entrypoint.sh
@@ -4,26 +4,28 @@ set -ex
 
 ROOTKEY=${ROOTKEY:-"/var/cache/knot-resolver/root.keys"}
 CACHE=${CACHE:-"/var/cache/knot-resolver"}
-GC_OPT=${GC_OPT:-"-d 1000"}
+#GC_OPT=${GC_OPT:-"-d 1000"}
 
 # Deleting the trust anchor key file
 if [ -e ${ROOTKEY} ]; then
     rm -f ${ROOTKEY}
 fi
 
+## Knot Resolver Garbage Collector daemon
+#sleep 10 &
+mkdir -p "$CACHE"
+kres-cache-gc -c ${CACHE} ${GC_OPT}
+
 # Knot Resolver daemon
 if [ "${1#-}" != "$1" ]; then
     #set -- /usr/local/sbin/kresd "$@"
-    exec kresd "$@" &
+    exec kresd "$@"
 else
-    exec "$@" &
+    exec "$@"
 fi
 #exec "$@" &
 
 
-# Knot Resolver Garbage Collector daemon
-sleep 10 &
-kres-cache-gc -c ${CACHE} ${GC_OPT}
 
 # process foreground
-fg %1
+#fg %1
