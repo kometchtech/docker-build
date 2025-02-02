91cf225b1f6527b475ae73820057463a916a216a
kometchtech <kometch@gmail.com>
2025-02-02 16:37:31 +0900

    fix bind
    * fix Dockerfile

--- bind/entrypoint.sh
+++ bind/entrypoint.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+set -e
 
-set -e 
+# 必要なディレクトリの権限を確認
+chown -R _named:_named /etc/named /var/cache/named /var/run/named /var/cache/named/dynamic
 
-if [ -z $# ]; then
-    echo "Starting named..."
-    $(which named) -c /etc/named/named.conf -g -u _named
-else
-    echo "Starting named..."
-    `$@`
+# named.conf が存在しない場合、デフォルトの設定をコピー
+if [ ! -f /etc/named/named.conf ]; then
+    cp /etc/named/named.conf.default /etc/named/named.conf
 fi
 
+# 環境変数から設定を動的に生成する機能をここに追加できます
+# 例: sed -i 's/REPLACE_WITH_FORWARDERS/'"${DNS_FORWARDERS}"'/' /etc/named/named.conf
 
-#tail -f /dev/null
-#exec "$@"
-exec $(which named) $@ 
+# 引数がある場合はそれを使用し、ない場合はデフォルトのコマンドを実行
+if [ $# -eq 0 ]; then
+    exec /usr/local/sbin/named -c /etc/named/named.conf -g -u _named
+else
+    exec "$@"
+fi
