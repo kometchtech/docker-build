bca51e8d25a97bef7e5839b68c143cd473bea86f
kometchtech <kometch@gmail.com>
2025-02-05 18:26:47 +0900

    fix nsd
    * fix Dockerfile LABEL
    * fix entrypoint.sh

--- nsd/entrypoint.sh
+++ nsd/entrypoint.sh
@@ -1,17 +1,32 @@
-#!/usr/bin/env sh
-
+#!/bin/bash
 set -e
 
-if [ ! -f /usr/local/etc/nsd_server.pem ]; then
-  nsd-control-setup
+# Function to update DNS zone files
+update_zones() {
+    for zone in /etc/nsd/zones/*.zone; do
+        if [ -f "$zone" ]; then
+            zone_name=$(basename "$zone" .zone)
+            nsd-control reload "$zone_name"
+        fi
+    done
+}
+
+# Check if we're running as root
+if [ "$(id -u)" = '0' ]; then
+    # Ensure NSD directories have correct permissions
+    chown -R nsd:nsd /var/cache/nsd /var/run/nsd /etc/nsd/zones
+    chmod 755 /var/cache/nsd /var/run/nsd /etc/nsd/zones
+
+    # If command starts with an option, prepend nsd
+    if [ "${1#-}" != "$1" ]; then
+        set -- nsd "$@"
+    fi
+
+#    # If the command is nsd, switch to the nsd user
+#    if [ "$1" = 'nsd' ]; then
+#        exec nsd "$@"
+#    fi
 fi
-# change owner and group
-#chown -R nsd:nsd /usr/local/etc/nsd /usr/local/etc/nsd/zones /usr/local/var/db/nsd /var/run/nsd
 
-if [ ! -d /var/run/nsd ]; then
-    mkdir -p /var/run/nsd
-fi
-
-$@
-
-tail -f /dev/null
+# Run command with exec to replace shell with the command
+exec "$@"
