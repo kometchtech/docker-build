3fa3bf6e3aaeb8f50990ace7a7fa07d36f4d31d9
kometchtech <kometch@gmail.com>
2025-03-21 16:52:21 +0900

    fix dnsdist/entrypoint.sh

--- dnsdist/entrypoint.sh
+++ dnsdist/entrypoint.sh
@@ -2,4 +2,14 @@
 
 set -e
 
-exec "$@"
+# デフォルトのコマンドとパラメータ
+DEFAULT_CMD="dnsdist --supervised --disable-syslog --log-timestamps -u _dnsdist -g _dnsdist --config /etc/dnsdist/dnsdist.conf"
+
+# 引数が渡されていれば使用し、なければデフォルトを使用
+if [ $# -eq 0 ]; then
+    # 引数がない場合はデフォルトコマンドを使用
+    exec $DEFAULT_CMD
+else
+    # 引数がある場合はそれを使用
+    exec "$@"
+fi
