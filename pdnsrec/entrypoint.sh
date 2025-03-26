8def6ceeb23291bbd7b1369182fb4b3c4d88bd44
kometchtech <kometch@gmail.com>
2025-03-26 15:21:14 +0900

    fix powerdns recursor
    * fix pdnsrec/Dockerfile
    * fix pdnsrec/entrypoint.sh

--- pdnsrec/entrypoint.sh
+++ pdnsrec/entrypoint.sh
@@ -1,11 +1,19 @@
-#!/usr/bin/env sh
+#!/bin/sh
+set -e
 
-set +e
-
-mkdir -p /var/run/pdns-recursor
-
-if [ "${1#-}" != "$1" ]; then
-    set -- pdns_recursor "$@"
+# Ensure runtime directories exist
+if [ ! -d "/var/run/pdns-recursor" ]; then
+  mkdir -p /var/run/pdns-recursor
+  chown pdns-recursor:pdns-rec /var/run/pdns-recursor
 fi
 
+# Set proper permissions for data directories
+chown -R pdns-recursor:pdns-rec /var/lib/pdns-recursor
+
+# Handle command line arguments
+if [ "${1#-}" != "$1" ]; then
+  set -- pdns_recursor "$@"
+fi
+
+# Execute the process
 exec "$@"
